4934d3db1bc2617cb7ab4e3757e2a305077f16ad
Karolina <kika.romanska@gmail.com>
2018-02-05 18:29:27 +0100

    Displaying end of the working time

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let countdown ;
+const timerDisplay = document.querySelector(".display_time-left");
+const endTime = document.querySelector('.display_time-end');
+
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
 
    countdown = setInterval(() => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -17,5 +21,17 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
     const minutes = Math.floor(seconds/60);
     const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
     console.log({minutes, remainderSeconds});
+}
+
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const display = `Be back at: ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+    endTime.textContent = display;
+
 }
